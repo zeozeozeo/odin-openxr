b2a63a80515f6af5e3c87a6181a137efe7b29fb1
Isabella Basso <isabellabdoamaral@gmail.com>
2026-05-27 19:44:49 -0300

    build: replace git clone --revision with checkout fallback

--- build.sh
+++ build.sh
@@ -2,7 +2,23 @@
 
 set -ex
 
-[ -d OpenXR-SDK ] || git clone --revision c15d38cb4bb10a5b7e075f74493ff13896e2597a https://github.com/KhronosGroup/OpenXR-SDK.git
+clone_at_revision() {
+    local dir="$1"
+    local revision="$2"
+    local remote="$3"
+    shift 3
+    [ -d "$dir" ] && return
+    git clone "$@" "$remote" "$dir"
+    if ! git -C "$dir" checkout --detach "$revision"; then
+        git -C "$dir" fetch origin "$revision"
+        git -C "$dir" checkout --detach FETCH_HEAD
+    fi
+    if [ -f "$dir/.gitmodules" ]; then
+        git -C "$dir" submodule update --init --recursive
+    fi
+}
+
+clone_at_revision OpenXR-SDK c15d38cb4bb10a5b7e075f74493ff13896e2597a https://github.com/KhronosGroup/OpenXR-SDK.git
 
 pushd OpenXR-SDK
 cmake -B build -DCMAKE_BUILD_TYPE=Release
